050923521024648f1e3ccedf4d935391330943bf
9WiSHao <825763548@qq.com>
2022-01-17 17:32:12 +0800

    以前作业更改

--- 2/lv3/js.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.i')['onmouseover']=x=>{
-    document.querySelector('.others').className = 'others2'
-    document.querySelector('.san').className = 'san2'
-}
-
-document.querySelector('.i')['onmouseout']=x=>{
-    document.querySelector('.others2').className = 'others'
-    document.querySelector('.san2').className = 'san'
-}
